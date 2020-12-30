2fe266c4e31dec41da1837b260fdd7d84d93f231
Robert Obi <robobi97@googlemail.com>
2020-12-30 04:26:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,7 @@
 def my_hash_creator(key, value)
-  {name: "Robert", characteristic: "Supreme"
+  {
+    name: "Robert", 
+  characteristic: "Supreme"
     }
   # return a hash that includes the key and value parameters passed into this method
 end
